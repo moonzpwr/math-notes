655c33c3296b3f52288729a52a543a7e2fb0fcc4
Andrew Yudenkov <yudenkovandre@gmail.com>
2025-06-17 16:27:20 +0300

    move sticky headers feature to separate branch

--- src/Components/SectionItem/SectionItem.tsx
+++ src/Components/SectionItem/SectionItem.tsx
@@ -3,28 +3,21 @@ import type { INotebookData } from "@/interfaces/INotebookData";
 import { RichParagraph } from "../RichParagraph/RichParagraph";
 import styles from './SectionItem.module.css'
 
+
 interface Props {
     title?: string;
     data: INotebookData[];
     id: string;
-    headersRef: React.MutableRefObject<Record<string, HTMLDivElement | null>>;
-    idPath: string[];
 }
 
 
-export const SectionItem: React.FC<Props> = ({ title, data, id, headersRef, idPath }) => {
+export const SectionItem: React.FC<Props> = ({ title, data, id }) => {
+
+
+
     return (
         <div className={styles.container}>
-            <div
-                className={styles.title}
-                ref={(el) => {
-                    headersRef.current[id] = el;
-                }}
-                data-id={id}
-                data-id-path={idPath.length > 0 ? idPath.join('.') : null}
-            >
-                {title}
-            </div>
+            <div className={styles.title} data-id={id}>{title}</div>
             <div>{data?.map((item, i) => {
                 if (item.type === StructureTypes.RichParagraph) {
                     return <RichParagraph key={i} content={item.value} />
@@ -33,10 +26,8 @@ export const SectionItem: React.FC<Props> = ({ title, data, id, headersRef, idPa
                         <SectionItem
                             key={item.id}
                             id={item.id}
-                            headersRef={headersRef}
                             title={item.title}
                             data={item.data}
-                            idPath={item.idPath || []}
                         />)
                 }
 
--- src/Views/NotebookView/NotebookView.tsx
+++ src/Views/NotebookView/NotebookView.tsx
@@ -3,8 +3,7 @@ import { SectionItem } from "../../Components/SectionItem/SectionItem";
 import { DataState } from "@/enums/DataState";
 import { LoadingSpinner } from "@/Components/LoadingSpinner/LoadingSpinner";
 import styles from './NotebookView.module.css';
-import { useEffect, useRef, useState } from "react";
-import { getAllHeaders } from "@/helpers/getAllHeaders";
+import { useEffect, useRef } from "react";
 
 
 export const NotebookView: React.FC = () => {
@@ -17,78 +16,19 @@ export const NotebookView: React.FC = () => {
     }, [state])
 
 
-    const [activeHeaders, setActiveHeaders] = useState<string[]>([]);
-    const headersRef = useRef<Record<string, HTMLDivElement | null>>({});
-
-    useEffect(() => {
-        const observer = new IntersectionObserver(
-            (entries) => {
-                entries.forEach((entry) => {
-                    const id = entry.target.getAttribute('data-id');
-                    if (!id) return;
-
-                    if (entry.boundingClientRect.top <= activeHeaders.length * 10) {
-                        const idPath = entry.target.getAttribute('data-id-path')?.split('.') || [];
-                        console.log(entry);
-                        setActiveHeaders([...idPath, id]);
-                    } else if (entry.boundingClientRect.top > activeHeaders.length * 43) {
-                        // const idPath = entry.target.getAttribute('data-id-path')?.split('.') || [];
-                        // console.log(entry);
-                        // setActiveHeaders((prev) => prev.filter((h) => h === id));
-                    }
-                });
-            },
-            {
-                threshold: [1],
-                // rootMargin: '0px 0px -90% 0px',
-            }
-        );
-
-        const headers = getAllHeaders(data?.data || [])
-        console.log('Headers:', headers);
-        headers.forEach((section) => {
-            if (!section.id) return;
-            const el = headersRef.current[section.id];
-            if (el) observer.observe(el);
-        });
-
-        return () => observer.disconnect();
-    }, [data]);
 
 
     return (
         <div className={styles.container}>
             {isLoading ? <div className={styles.spinnerContainer}><LoadingSpinner /></div> :
                 <>
-                    <div style={{ position: 'fixed', top: 0, zIndex: 999, width: '100%', }}>
-                        {activeHeaders.map((id, i) => (
-                            <div
-                                key={id}
-                                style={{
-                                    height: 40,
-                                    background: 'white',
-                                    borderBottom: '1px solid #ccc',
-                                    padding: '0 12px',
-                                    lineHeight: '40px',
-                                    position: 'sticky',
-                                    top: `${i * 40}px`,
-                                    width: '100%',
-                                }}
-                                id={id}
-                            >
-                                {getAllHeaders(data?.data || []).find((s) => s.id === id)?.title}
-                            </div>
-                        ))}
-                    </div>
                     <h1 style={{ textAlign: 'center' }} ref={titleRef}>{data?.title}</h1>
                     {data?.data?.map((item) => item.data && (
                         <SectionItem
-                            headersRef={headersRef}
                             key={item.id}
                             id={item.id}
                             title={item.title}
                             data={item.data}
-                            idPath={item.idPath || []}
                         />
                     ))}
                 </>}
